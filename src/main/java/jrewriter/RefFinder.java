e7cbeee2137b3e2183408be25f21b2bc443f7208
maomaohaoyde <maomaohaoyde@hotmail.com>
2018-03-06 16:18:14 -0800

    Implements incrementRef

--- src/main/java/jrewriter/RefFinder.java
+++ src/main/java/jrewriter/RefFinder.java
@@ -39,12 +39,57 @@ public class RefFinder {
                 }
             }
         }
+    }
+
 
         // if this method operates on the same object that it
         // references the field from, do nothing
-    }
 
-    public static void incrementRef() throws BadBytecode {
+
+    public static void incrementRef(CtClass cc) throws BadBytecode {
         // TODO: code to identify sequence of bytecode that increments field
+        ClassFile cf = cc.getClassFile();
+        ConstPool constPool = cf.getConstPool();
+
+        List<MethodInfo> methods = cf.getMethods();
+
+        for (MethodInfo minfo : methods) {
+            CodeAttribute ca = minfo.getCodeAttribute();
+            CodeIterator ci = ca.iterator();
+            CodeIterator temp = ca.iterator();
+
+            while (ci.hasNext()) {
+                ci.next();
+                int index = temp.next();
+
+                int constPoolIndex;
+                int startIndex;
+                int endIndex;
+
+                if (temp.byteAt(index) == Opcode.GETFIELD)
+                {   
+                    startIndex = index;
+                    constPoolIndex = temp.u16bitAt(index+1);
+                    System.out.println("getfield "+constPoolIndex);
+                    index = temp.next();
+                    if (temp.byteAt(index) == Opcode.ICONST_1)
+                    {
+                        System.out.println("ICONST_1");
+                        index = temp.next();
+                        if (temp.byteAt(index) == Opcode.IADD)
+                        {
+                            System.out.println("IADD");
+                            index = temp.next();
+                            if (temp.byteAt(index) == Opcode.PUTFIELD) {
+                                if (constPoolIndex==temp.u16bitAt(index+1)) {
+                                    System.out.println("putfield "+constPoolIndex);
+                                    endIndex = temp.next();
+                                }
+                            }
+                        }
+                    }
+                }   
+            }
     }
+    } 
 }
